9ed590db3af29ae62cb5dc206ca119350c03afb9
Hugo Gómez Martín <hugogmartin@hotmail.com>
2025-03-14 19:32:23 +0100

    interfaz ChatClient realizada y añadido el esqueleto de la clase ChatClientImpl.java

--- ChatPractica1/src/main/java/es/ubu/lsi/client/ChatClient.java
+++ ChatPractica1/src/main/java/es/ubu/lsi/client/ChatClient.java
@@ -1,5 +1,12 @@
 package es.ubu.lsi.client;
+import es.ubu.lsi.common.*;
 
 public interface ChatClient {
+	
+	boolean start();
+	
+	void sendMessage(ChatMessage msg);
+	
+	void disconnect();
 
 }
--- ChatPractica1/src/main/java/es/ubu/lsi/client/ChatClientImpl.java
+++ ChatPractica1/src/main/java/es/ubu/lsi/client/ChatClientImpl.java
@@ -1,7 +1,21 @@
 package es.ubu.lsi.client;
 
+import es.ubu.lsi.common.ChatMessage;
+
 public class ChatClientImpl implements ChatClient {
 	
+	private String server;
+	private String username;
+	private static int port = 1500;
+	private boolean carryOn = true;
+	private int id;
+	
+	
+	public ChatClientImpl(String server, int port, String username) {
+		this.server = server;
+		this.port = port;
+		this.username = username;
+	}
 	
 	private class ChatClientListener implements Runnable {
         @Override
@@ -9,5 +23,27 @@ public class ChatClientImpl implements ChatClient {
             
         }
     }
+
+	@Override
+	public boolean start() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void sendMessage(ChatMessage msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void disconnect() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
