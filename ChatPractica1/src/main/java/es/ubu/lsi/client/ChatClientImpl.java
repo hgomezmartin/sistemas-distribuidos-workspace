10974a35e5f98cdf97c0cb56b106493394c1dd9a
Hugo Gómez Martín <hugogmartin@hotmail.com>
2025-03-14 18:50:12 +0100

    Añadida la estructura inicial de paquetes y módulos

---
+++ ChatPractica1/src/main/java/es/ubu/lsi/client/ChatClientImpl.java
@@ -0,0 +1,13 @@
+package es.ubu.lsi.client;
+
+public class ChatClientImpl implements ChatClient {
+	
+	
+	private class ChatClientListener implements Runnable {
+        @Override
+        public void run() {
+            
+        }
+    }
+
+}
---
+++ ChatPractica1/src/main/java/es/ubu/lsi/server/ChatServerImpl.java
@@ -0,0 +1,12 @@
+package es.ubu.lsi.server;
+
+public class ChatServerImpl implements ChatServer{
+	
+	private class ServerThreadForClient extends Thread {
+        @Override
+        public void run() {
+            
+        }
+    }
+
+}
